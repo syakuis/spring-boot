2db8b91ac0f4670eca252ee57c268a1097f20e51
최석균 <syaku@naver.com>
2018-07-02 16:31:07 +0900

    target 파라메터를 http request.getParameterMap() 형식에 맞게 수정.

--- src/test/java/org/syaku/springboot/web/utils/ParameterUtilsTest.java
+++ src/test/java/org/syaku/springboot/web/utils/ParameterUtilsTest.java
@@ -4,42 +4,66 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
-import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.springframework.util.StringUtils;
 
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * http request parameter 를 제어하는 클래스 테스트
- * merge : 두개의 파라메터를 병합한다.
- * pick : 대상이 되는 파라메터에서 해당 요소만 가져온다.
  * @author Seok Kyun. Choi. 최석균 (Syaku)
- * @since 2018. 6. 29.
+ * @since 2018. 7. 2.
  */
 public class ParameterUtilsTest {
-  Map<String, String> params = new LinkedHashMap<>();
+  Map<String, String[]> params = new LinkedHashMap<>();
 
-  @Before
-  public void setup() {
-    params.put("page", "1");
-    params.put("search", "choi");
+  @Test
+  public void 기능_테스트() {
+    // mapToString test
+    params.put("page", new String[]{"1"});
+    params.put("search", new String[]{"choi"});
+    assertEquals(ParameterUtils.mapToString(params), "page=1&search=choi");
+    params.put("search", new String[]{"choi","choi2"});
+    assertEquals(ParameterUtils.mapToString(params), "page=1&search=choi&search=choi2");
+    assertEquals(ParameterUtils.mapToString(Collections.emptyMap()), "");
+
+    // stringToMap test
+    // 문자열로 변경하여 비교한다.
+    assertEquals(ParameterUtils.mapToString(ParameterUtils.stringToMap("page=1&search=choi&search=choi2")),
+      "page=1&search=choi&search=choi2");
+
+    assertEquals(ParameterUtils.mapToString(ParameterUtils.stringToMap("page=1&search="), true),
+      "page=1&search=");
+    assertEquals(ParameterUtils.mapToString(ParameterUtils.stringToMap("page=1&search=")),
+      "page=1");
+
+    assertEquals(ParameterUtils.stringToMap(""), Collections.emptyMap());
   }
 
   @Test
-  public void 병합_테스트() {
-    assertEquals(ParameterUtils.mapToString(params), "page=1&search=choi");
-    assertEquals(ParameterUtils.stringToMap("page=1&search=choi"), params);
+  public void 합집합_테스트() {
+    // union test
+    params.put("page", new String[]{"2"});
+    params.put("search", new String[]{"choi"});
+    params.put("mode", new String[]{"save"});
 
-    Map<String, String> actual = new LinkedHashMap<>();
-    actual.put("page", "2");
-    actual.put("search", "");
-    actual.put("mode", "save");
-    assertEquals(ParameterUtils.union(params, "page=2&search=&mode=save"), actual);
+    Map<String, String[]> actual = new LinkedHashMap<>(params);
+    actual.put("search", new String[]{});
+
+    // 문자열로 변경하여 비교한다.
+    assertEquals(ParameterUtils.mapToString(ParameterUtils.union(params, "page=2&search=&mode=save")),
+      ParameterUtils.mapToString(actual));
+    assertEquals(ParameterUtils.mapToString(ParameterUtils.union(null, "page=2&search=&mode=save")),
+      ParameterUtils.mapToString(actual));
+    assertEquals(ParameterUtils.mapToString(
+      ParameterUtils.union(Collections.emptyMap(), "page=2&search=&mode=save")),
+      ParameterUtils.mapToString(actual));
+  }
+
+  @Test
+  public void 합치기_테스트() {
+    params.put("page", new String[]{"1"});
+    params.put("search", new String[]{"choi"});
 
     assertEquals(ParameterUtils.merge(params, "mode=save"), "page=1&search=choi&mode=save");
     assertEquals(ParameterUtils.merge(params, "page=1"), "page=1&search=choi");
@@ -54,6 +78,9 @@ public class ParameterUtilsTest {
 
   @Test
   public void 꺼내기_테스트() {
+    params.put("page", new String[]{"1"});
+    params.put("search", new String[]{"choi"});
+
     assertEquals(ParameterUtils.pick(params, "page="), "page=1");
     assertEquals(ParameterUtils.pick(params, "page=&search=choi"), "page=1&search=choi");
     assertEquals(ParameterUtils.pick(params, "test=man&page=&search=choi"), "test=man&page=1&search=choi");
@@ -61,6 +88,7 @@ public class ParameterUtilsTest {
     assertEquals(ParameterUtils.pick(null, "test=man&page=&search=choi"), "test=man&search=choi");
   }
 }
+
 
 @Slf4j
 final class ParameterUtils {
@@ -90,18 +118,41 @@ final class ParameterUtils {
    * @param params string parameter type
    * @return parameter map
    */
-  public static Map<String, String> stringToMap(String params) {
-    Map<String, String> result = new LinkedHashMap<>();
+  public static Map<String, String[]> stringToMap(String params) {
+    if (params == null || params.length() == 0) {
+      return Collections.emptyMap();
+    }
+    Map<String, List<String>> store = new LinkedHashMap<>();
     String[] pairs = params.split("&");
 
     try {
       for (String pair : pairs) {
         int idx = pair.indexOf("=");
+        String name = URLDecoder.decode(pair.substring(0, idx), ParameterUtils.charset);
         String value = URLDecoder.decode(pair.substring(idx + 1), ParameterUtils.charset);
-        result.put(URLDecoder.decode(pair.substring(0, idx), ParameterUtils.charset), value);
+
+        if (store.containsKey(name)) {
+          List<String> values = store.get(name);
+          if (value != null && value.length() > 0) {
+            values.add(value);
+          }
+          store.put(name, values);
+        } else {
+          List<String> values = new LinkedList<>();
+          if (value != null && value.length() > 0) {
+            values.add(value);
+          }
+          store.put(name, values);
+        }
       }
     } catch (UnsupportedEncodingException e) {
       log.error(e.getMessage());
+    }
+
+    Map<String, String[]> result = new LinkedHashMap<>();
+
+    for (Map.Entry<String, List<String>> values: store.entrySet()) {
+      result.put(values.getKey(), values.getValue().toArray(new String[values.getValue().size()]));
     }
 
     return result;
@@ -111,7 +162,7 @@ final class ParameterUtils {
    * @param target 대상이될 파라메터 맵
    * @return string parameter
    */
-  public static String mapToString(Map<String, String> target) {
+  public static String mapToString(Map<String, String[]> target) {
     return mapToString(target, false);
   }
 
@@ -121,18 +172,26 @@ final class ParameterUtils {
    * @param allowEmpty 빈값을 만들지 여부 기본적으로 만들지 않는 다.
    * @return parameter string
    */
-  public static String mapToString(Map<String, String> target, boolean allowEmpty) {
+  public static String mapToString(Map<String, String[]> target, boolean allowEmpty) {
     if (target == null || target.isEmpty()) {
       return "";
     }
 
     StringBuilder result = new StringBuilder();
-    for (Map.Entry<String, String> param : target.entrySet()) {
-      String value = param.getValue();
-      if (!allowEmpty && isEmpty(value)) {
+    for (Map.Entry<String, String[]> map : target.entrySet()) {
+      String[] values = map.getValue();
+      if (!allowEmpty && (values == null || values.length == 0)) {
         continue;
       }
-      result.append(param.getKey()).append("=").append(value).append("&");
+
+      if (values == null || values.length == 0) {
+        result.append(map.getKey()).append("=&");
+        continue;
+      }
+
+      for (String value : values) {
+        result.append(map.getKey()).append("=").append(value).append("&");
+      }
     }
 
     return result.toString().replaceAll("&$", "");
@@ -144,8 +203,8 @@ final class ParameterUtils {
    * @param value 새로운 맵
    * @return parameter map
    */
-  public static Map<String, String> union(Map<String, String> target, String value) {
-    Map<String, String> result;
+  public static Map<String, String[]> union(Map<String, String[]> target, String value) {
+    Map<String, String[]> result;
 
     if (target == null || target.isEmpty()) {
       result = new LinkedHashMap<>();
@@ -160,11 +219,11 @@ final class ParameterUtils {
   /**
    * 대상이 되는 파라매터 맵에 문자열 파라메터 구문을 이용하여 문자열 파라메터를 반환한다.
    * @param target parameter map
-   * @param value parameter string
+   * @param query parameter query string
    * @return parameter string
    */
-  public static String merge(Map<String, String> target, String value) {
-    return merge(target, value, false);
+  public static String merge(Map<String, String[]> target, String query) {
+    return merge(target, query, false);
   }
 
   /**
@@ -177,33 +236,51 @@ final class ParameterUtils {
    * (page=1&search=choi , page=2&search=&mode=save) return "page=2&mode=save"
    * (page=1&search=choi , mode=save) return "page=1&search=choi&mode=save"
    * @param target parameter map
-   * @param value parameter string
+   * @param query parameter query string
    * @param allowEmpty 빈값을 가진 파라메터를 제거할지 여부를 설정한다. mapToString 에서 사용된다.
    * @return parameter string
    */
-  public static String merge(Map<String, String> target, String value, boolean allowEmpty) {
-    return mapToString(union(target, value), allowEmpty);
+  public static String merge(Map<String, String[]> target, String query, boolean allowEmpty) {
+    return mapToString(union(target, query), allowEmpty);
   }
+
+  private static String mapToLog(Map<String, String[]> target) {
+    StringBuilder builder = new StringBuilder();
+    for (Map.Entry<String, String[]> map : target.entrySet()) {
+      if (map.getValue() == null || map.getValue().length == 0) {
+        builder.append(map.getKey()).append("=&");
+      } else {
+        for (String value : map.getValue()) {
+          builder.append(map.getKey()).append("=").append(value).append("&");
+        }
+      }
+    }
+
+    String result = builder.toString().replaceAll("&$", "");
+    log.debug(result);
+    return result;
+  }
+
 
   /**
    *
    * @param target
-   * @param value
+   * @param query
    * @return
    */
-  public static String pick(Map<String, String> target, String value) {
-    Map<String, String> values = stringToMap(value);
+  public static String pick(Map<String, String[]> target, String query) {
+    Map<String, String[]> result = stringToMap(query);
 
     if (target == null || target.isEmpty()) {
-      return mapToString(values);
+      return mapToString(result);
     }
 
-    for (Map.Entry<String, String> item : values.entrySet()) {
-      if (StringUtils.isEmpty(item.getValue())) {
-        values.put(item.getKey(), target.get(item.getKey()));
+    for (Map.Entry<String, String[]> map : result.entrySet()) {
+      if (map.getValue() == null || map.getValue().length == 0) {
+        result.put(map.getKey(), target.get(map.getKey()));
       }
     }
 
-    return mapToString(values);
+    return mapToString(result);
   }
 }
